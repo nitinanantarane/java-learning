abd8b97c95375054e655d50a0e47b346717e5ca9
nitin rane <nitinanantarane@gmail.com>
2022-08-24 17:32:15 +0530

    method references

---
+++ src/main/java/com/nitinrane/learning/java/functional/ComparatorTest.java
@@ -0,0 +1,18 @@
+package com.nitinrane.learning.java.functional;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.naturalOrder;
+
+public class ComparatorTest {
+
+    public static void main(String[] args) {
+        List<String> numbers = Arrays.asList("one", null, "two", null, "three", null, "four");
+        Comparator<String> naturalNullLast = Comparator.nullsLast(naturalOrder());
+        numbers.sort(naturalNullLast);
+        System.out.println(numbers);
+
+    }
+}
---
+++ src/main/java/com/nitinrane/learning/java/functional/ConstructorMethodRef.java
@@ -0,0 +1,34 @@
+package com.nitinrane.learning.java.functional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ConstructorMethodRef {
+
+    public static void main(String[] args) {
+        Supplier<List<String>> supplier = ArrayList::new;
+        System.out.println(supplier.get());
+
+        Supplier<User> userSupplier = () -> new User("Hello");
+        System.out.println(userSupplier.get());
+    }
+}
+
+class User {
+    String name;
+    User(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "name='" + name + '\'' +
+            '}';
+    }
+}
